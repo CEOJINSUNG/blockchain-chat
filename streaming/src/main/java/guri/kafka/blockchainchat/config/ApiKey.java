a72ae1a30d0f6baa3da9017b75932209146cb42e
CEOJINSUNG <jinsung1048@gmail.com>
2022-05-14 23:06:53 +0900

    api key/secret key 누출 방지

---
+++ streaming/src/main/java/guri/kafka/blockchainchat/config/ApiKey.java
@@ -0,0 +1,28 @@
+package guri.kafka.blockchainchat.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Component
+public class ApiKey {
+    @Value("${api-key}")
+    private String apikey;
+    @Value("${secret-key}")
+    private String secretkey;
+
+    private static ApiKey instance = new ApiKey();
+
+    private ApiKey() {
+
+    }
+
+    public static ApiKey getInstance() {
+        if (instance == null) {
+            instance = new ApiKey();
+        }
+        return instance;
+    }
+}
