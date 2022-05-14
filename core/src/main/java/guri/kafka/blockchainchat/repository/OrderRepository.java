135196842f0f4c7b5d5a4f290bc235f80f8978ab
CEOJINSUNG <jinsung1048@gmail.com>
2022-05-15 01:24:00 +0900

    Update OrderRepository.java

--- core/src/main/java/guri/kafka/blockchainchat/repository/OrderRepository.java
+++ core/src/main/java/guri/kafka/blockchainchat/repository/OrderRepository.java
@@ -1,9 +1,17 @@
 package guri.kafka.blockchainchat.repository;
 
 import guri.kafka.blockchainchat.domain.Order;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Order> findById(Long id);
+
 }
